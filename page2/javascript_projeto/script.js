8b025c6efd2ab836f9b52c7175f0e7c3fd183c78
Thais Ferreira <96101823+thaisjunges@users.noreply.github.com>
2023-05-17 00:03:06 -0300

    Add files via upload

--- page2/javascript_projeto/script.js
+++ page2/javascript_projeto/script.js
@@ -1,22 +1,24 @@
 function carregar() {
-    // CHAMANDO ID DO HTML
-var msg = document.getElementById('msg')
-var img = dcument.getElementById('imagem')
-var data = new Date()
-var hora = data.getHours()
-
-msg.innerHTML = `Agora são ${hora} horas.`
-if (hora >= 0 && hora < 12) {
-    //Bom Dia!
-    img.src = 'manha.png'
-    document.body.style.background = '#e2cd9'
-} else if (hora >= 12 && hora <= 18) {
-    //Boa Tarde
-    img.src = 'tarde.png'
-    document.body.style.background = '#b9846f'
-} else {
-    //Boa Noite!
-    img.src = 'noite.png'
-    body.body.style.background = '#515154'
-}
+    //BUSCA MENSAGEM DO HTML
+    var msg = window.document.getElementById('msg')
+    //BUSCA A IMAGEM DO HTML
+    var img = window.document.getElementById('imagem')
+    //DATA ATUAL
+    var data = new Date()
+    //HORA ATUAL
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'img/manha.png'
+        document.body.style.background = '#d89e68'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE!
+        img.src = 'img/tarde.png'
+        document.body.style.background = 'b4b23463'
+    } else {
+        //BOA NOITE
+        img.src = 'img/noite.png'
+        document.body.style.background = '#1615158a'
+    }
 }
